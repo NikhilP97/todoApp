6c88c00e8e046612ce59e80b86d08fb29d52201f
Nikhil Pinto <nikhiljpinto@gmail.com>
2019-05-15 12:37:57 +0530

    styled login and register screen

--- src/Login.js
+++ src/Login.js
@@ -3,12 +3,13 @@ import { withRouter } from 'react-router-dom';
 import { Container, Flex, Box, Input, Button, Subhead, Text } from 'rebass';
 import firebase from './firebase';
 import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom';
+import classnames from "classnames";
 
 class Login extends Component {
   state = {
     email: '',
     password: '',
-    error: null,
+    error: {},
   };
 
   handleInputChange = (event) => {
@@ -32,7 +33,72 @@ class Login extends Component {
   render() {
     const { email, password, error } = this.state;
     return (
-      <Container>
+
+      <div className="container">
+        <div style={{ marginTop: "4rem" }} className="row">
+          <div className="col s8 offset-s2">
+            
+            <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+              <h4>
+                <b>Login</b> below
+              </h4>
+              <p className="grey-text text-darken-1">
+                Don't have an account? <NavLink to="/register">Register</NavLink>
+              </p>
+            </div>
+            <form onSubmit={this.handleSubmit}>
+              <div className="input-field col s12">
+                <input
+                  type="text"
+                  name="email" 
+                   
+                  value={email} 
+                  onChange={this.handleInputChange}
+                  
+                />
+                <label htmlFor="email">Email</label>
+              </div>
+              <div className="input-field col s12">
+                <input
+                  type="password"
+                  name="password"
+                  
+                  value={password}
+                  onChange={this.handleInputChange}
+                />
+                <label htmlFor="password">Password</label>
+                <span className="red-text">
+                  {error.message}
+                </span>
+              </div>
+              <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+                <button
+                  style={{
+                    width: "150px",
+                    borderRadius: "3px",
+                    letterSpacing: "1.5px",
+                    marginTop: "1rem"
+                  }}
+                  type="submit"
+                  className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+                >
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      
+    );
+  }
+}
+
+export default withRouter(Login);
+
+
+/*
+<Container>
         <Flex>
           <Box>
             <Subhead>Log In</Subhead>
@@ -73,8 +139,4 @@ class Login extends Component {
           </Box>
         </Flex>
       </Container>
-    );
-  }
-}
-
-export default withRouter(Login);
+*/
--- src/Register.js
+++ src/Register.js
@@ -7,9 +7,11 @@ import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-do
 
 class Register extends Component {
   state = {
+    firstname:'',
+    lastname:'',
     email: '',
     password: '',
-    error: null,
+    error: {},
   };
 
   handleInputChange = (event) => {
@@ -33,7 +35,94 @@ class Register extends Component {
   render() {
     const { email, password, error } = this.state;
     return (
-      <Container>
+
+      <div className="container">
+        <div style={{ marginTop: "4rem" }} className="row">
+          <div className="col s8 offset-s2">
+            <NavLink to="/login" className="btn-flat waves-effect">
+              <i className="material-icons left">keyboard_backspace</i> Back to
+              home
+            </NavLink>
+            <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+              <h4>
+                <b>Register</b> your account
+              </h4>
+              <p className="grey-text text-darken-1">
+                Already have an account? <NavLink to="/login">Log In</NavLink>
+              </p>
+            </div>
+            <form onSubmit={this.handleSubmit}>
+              <div className="input-field col s12">
+                <input
+                  type="text"
+                  name="firstname" 
+                  
+                  value={this.state.firstname} 
+                  onChange={this.handleInputChange} 
+                />
+                <label htmlFor="email">First Name</label>
+              </div>
+              <div className="input-field col s12">
+                <input
+                  type="text"
+                  name="lastname" 
+                   
+                  value={this.state.lastname} 
+                  onChange={this.handleInputChange} 
+                />
+                <label htmlFor="email">Last Name</label>
+              </div>
+              <div className="input-field col s12">
+                <input
+                  type="text"
+                  name="email" 
+                   
+                  value={email} 
+                  onChange={this.handleInputChange} 
+                />
+                <label htmlFor="email">Email</label>
+              </div>
+              <div className="input-field col s12">
+                <input
+                  type="password"
+                  name="password"
+                  
+                  value={password}
+                  onChange={this.handleInputChange}
+                />
+                <label htmlFor="password">Password</label>
+                <span className="red-text">
+                  {error.message}
+                </span>
+              </div>
+              <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+                <button
+                  style={{
+                    width: "150px",
+                    borderRadius: "3px",
+                    letterSpacing: "1.5px",
+                    marginTop: "1rem"
+                  }}
+                  type="submit"
+                  className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+                >
+                  Register
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      
+    );
+  }
+}
+
+export default withRouter(Register);
+
+
+/*
+<Container>
         <Flex>
           <Box>
             <Subhead>Register</Subhead>
@@ -74,8 +163,5 @@ class Register extends Component {
           </Box>
         </Flex>
       </Container>
-    );
-  }
-}
 
-export default withRouter(Register);
+*/
